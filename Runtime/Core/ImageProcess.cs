3d3647823f1f96e67bc9743d1b33d077af2f9804
Ivan Ivanovski <ivanivanovski86@gmail.com>
2023-01-25 11:11:20 +0100

    Update ImageProcess.cs

--- Runtime/Core/ImageProcess.cs
+++ Runtime/Core/ImageProcess.cs
@@ -73,7 +73,7 @@ namespace AsyncImageLibrary
                 TextureFormat.RGBA32 : TextureFormat.BGRA32;
             Texture2D texture = new Texture2D(asyncImage.Bitmap.Width, asyncImage.Bitmap.Height, textureFormat, false);
             texture.LoadRawTextureData(asyncImage.Bitmap.GetPixels(), asyncImage.Bitmap.RowBytes * asyncImage.Bitmap.Height);
-            texture.Apply(false, true);
+            texture.Apply(false, false);
             asyncImage.Texture = texture;
             onComplete?.Invoke();
         }
